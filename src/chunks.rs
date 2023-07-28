2db43c8a6ad5a3613c42114bced1b99e20c23bb3
CodeNil <codenil@proton.me>
2023-07-28 04:17:31 +0100

    Cleaned chunk code further

--- src/chunks.rs
+++ src/chunks.rs
@@ -1,12 +1,14 @@
-use crate::subdivision::{self, chunk_render};
+use crate::subdivision::{chunk_render, Chunk};
 use crate::world_noise;
 use bevy::prelude::*;
+use rayon::prelude::*;
 use std::collections::VecDeque;
 
 pub const CHUNK_SIZE: usize = 4;
 const RENDER_DISTANCE: usize = 16;
 
 /// Chunk search algorithm to generate chunks around the player
+#[allow(clippy::cast_precision_loss)]
 pub fn chunk_search(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
@@ -18,54 +20,47 @@ pub fn chunk_search(
     let data_generator = world_noise::DataGenerator::new();
 
     // Initialize state
-    let mut state = ChunkSearchState::new();
+    let mut queue = VecDeque::new();
+    let mut visited =
+        vec![vec![vec![false; RENDER_DISTANCE * 2]; RENDER_DISTANCE * 2]; RENDER_DISTANCE * 2];
 
-    while let Some(chunk) = state.queue.pop_front() {
-        explore_chunk(
-            &mut state,
-            &mut commands,
-            &mut meshes,
-            &mut materials,
+    queue.push_back((0, 0, 0));
+    visited[RENDER_DISTANCE / 2][RENDER_DISTANCE / 2][RENDER_DISTANCE / 2] = true;
+
+    let mut chunks_to_spawn = Vec::new();
+    while let Some(chunk) = queue.pop_front() {
+        chunks_to_spawn.append(&mut explore_chunk(
+            &mut visited,
+            &mut queue,
             &data_generator,
             chunk,
-        );
+        ));
     }
 
-    println!(
-        "Total: {total} Cubes: {cubes} Triangles: {triangles}",
-        total = state.total,
-        cubes = state.cubes,
-        triangles = state.triangles
-    );
+    // After all chunks have been explored, spawn them
+    let total = chunks_to_spawn.len();
+    let mut cubes = 0;
+    let mut triangles = 0;
+    for (chunk, neighbor) in chunks_to_spawn {
+        commands.spawn(PbrBundle {
+            mesh: meshes.add(chunk.mesh),
+            material: materials.add(StandardMaterial {
+                base_color: Color::WHITE,
+                ..default()
+            }),
+            transform: Transform::from_xyz(
+                neighbor.0 as f32 * CHUNK_SIZE as f32,
+                neighbor.1 as f32 * CHUNK_SIZE as f32,
+                neighbor.2 as f32 * CHUNK_SIZE as f32,
+            ),
+            ..Default::default()
+        });
+        cubes += chunk.n_cubes;
+        triangles += chunk.n_triangles;
+    }
+
+    println!("Total: {total} Cubes: {cubes} Triangles: {triangles}");
     println!("Time: {:#?}", start.elapsed());
-}
-
-/// Contains state for chunk search
-struct ChunkSearchState {
-    queue: VecDeque<(i32, i32, i32)>,
-    visited: Vec<Vec<Vec<bool>>>,
-    total: usize,
-    cubes: usize,
-    triangles: usize,
-}
-
-impl ChunkSearchState {
-    fn new() -> Self {
-        let mut queue = VecDeque::new();
-        let mut visited =
-            vec![vec![vec![false; RENDER_DISTANCE * 2]; RENDER_DISTANCE * 2]; RENDER_DISTANCE * 2];
-
-        queue.push_back((0, 0, 0));
-        visited[RENDER_DISTANCE / 2][RENDER_DISTANCE / 2][RENDER_DISTANCE / 2] = true;
-
-        Self {
-            queue,
-            visited,
-            total: 0,
-            cubes: 0,
-            triangles: 0,
-        }
-    }
 }
 
 /// Function to handle exploration of each chunk
@@ -74,13 +69,11 @@ impl ChunkSearchState {
 #[allow(clippy::cast_possible_wrap)]
 #[allow(clippy::cast_sign_loss)]
 fn explore_chunk(
-    state: &mut ChunkSearchState,
-    commands: &mut Commands,
-    meshes: &mut ResMut<Assets<Mesh>>,
-    materials: &mut ResMut<Assets<StandardMaterial>>,
+    visited: &mut Vec<Vec<Vec<bool>>>,
+    queue: &mut VecDeque<(i32, i32, i32)>,
     data_generator: &world_noise::DataGenerator,
     (chunk_x, chunk_y, chunk_z): (i32, i32, i32),
-) {
+) -> Vec<(Chunk, (i32, i32, i32))> {
     let directions = [
         (-1, 0, 0),
         (1, 0, 0),
@@ -89,6 +82,8 @@ fn explore_chunk(
         (0, 0, -1),
         (0, 0, 1),
     ];
+
+    let mut chunks_to_spawn = Vec::new();
 
     for &direction in &directions {
         let neighbor = (
@@ -109,11 +104,11 @@ fn explore_chunk(
             || voxel.0 >= RENDER_DISTANCE as i32 * 2
             || voxel.1 >= RENDER_DISTANCE as i32 * 2
             || voxel.2 >= RENDER_DISTANCE as i32 * 2;
-        let is_visited = state.visited[voxel.0 as usize][voxel.1 as usize][voxel.2 as usize];
+        let is_visited = visited[voxel.0 as usize][voxel.1 as usize][voxel.2 as usize];
         if is_out_of_bounds || is_visited {
             continue;
         }
-        state.visited[voxel.0 as usize][voxel.1 as usize][voxel.2 as usize] = true;
+        visited[voxel.0 as usize][voxel.1 as usize][voxel.2 as usize] = true;
 
         let render_result = chunk_render(
             data_generator,
@@ -126,41 +121,12 @@ fn explore_chunk(
         );
         let blocking = render_result.n_cubes == 1;
 
-        state.cubes += render_result.n_cubes;
-        state.triangles += render_result.n_triangles;
-
-        spawn_chunk(commands, meshes, materials, render_result, neighbor);
-
-        state.total += 1;
+        chunks_to_spawn.push((render_result, neighbor));
 
         if !blocking {
-            state.queue.push_back(neighbor);
+            queue.push_back(neighbor);
         }
     }
-}
 
-/// Function to spawn a chunk
-#[allow(clippy::cast_precision_loss)]
-fn spawn_chunk(
-    commands: &mut Commands,
-    meshes: &mut ResMut<Assets<Mesh>>,
-    materials: &mut ResMut<Assets<StandardMaterial>>,
-    render_result: subdivision::Chunk,
-    neighbor: (i32, i32, i32),
-) {
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(render_result.mesh),
-        material: materials.add(StandardMaterial {
-            base_color: Color::WHITE,
-            metallic: 0.8,
-            perceptual_roughness: 0.3,
-            ..default()
-        }),
-        transform: Transform::from_xyz(
-            neighbor.0 as f32 * CHUNK_SIZE as f32,
-            neighbor.1 as f32 * CHUNK_SIZE as f32,
-            neighbor.2 as f32 * CHUNK_SIZE as f32,
-        ),
-        ..Default::default()
-    });
+    chunks_to_spawn
 }
